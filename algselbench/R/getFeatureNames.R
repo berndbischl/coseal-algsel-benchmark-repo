787b1f5a809587a1b6cd3be9f369e95e4d4d9fa4
Bernd Bischl <bernd_bischl@gmx.net>
2013-12-20 14:42:25 +0100

    more work on R code

---
+++ algselbench/R/getFeatureNames.R
@@ -0,0 +1,3 @@
+getFeatureNames = function(astask) {
+  c(astask$desc$features_deterministic, astask$desc$features_stochastic)
+}
--- algselbench/R/parseASTask.R
+++ algselbench/R/parseASTask.R
@@ -9,11 +9,13 @@ parseASTask = function(path) {
   checkArg(path, "character", len = 1L, na.ok = FALSE)
 
   desc = parseDescription(path)
+  feature.runstatus = read.arff(file.path(path, "feature_runstatus.arff"))
   feature.values = read.arff(file.path(path, "feature_values.arff"))
   algo.runs = read.arff(file.path(path, "algorithm_runs.arff"))
 
   makeS3Obj("ASTask",
     desc = desc,
+    feature.runstatus = feature.runstatus,
     feature.values = feature.values,
     algo.runs = algo.runs
   )
