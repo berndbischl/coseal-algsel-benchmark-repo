a70ebce74d0e6ae46680a61f392b3d5d6b65fe3d
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-02-05 17:57:56 +0100

    clean up

--- algselbench/R/getFeatureNames.R
+++ algselbench/R/getFeatureNames.R
@@ -1,3 +1,9 @@
+#' Returns feature names of task.
+#'
+#' @param astask [\code{\link{ASTask}}]\cr
+#'   Algorithm selection task.
+#' @return [\code{character}].
+#' @export
 getFeatureNames = function(astask) {
   c(astask$desc$features_deterministic, astask$desc$features_stochastic)
 }
