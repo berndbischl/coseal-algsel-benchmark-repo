d13d1043b65f678b6ff0e5b898ccc18f1b843367
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-02-05 21:54:54 +0100

    clean up

--- algselbench/R/parseDescription.R
+++ algselbench/R/parseDescription.R
@@ -2,15 +2,15 @@
 
 parseDescription = function(path) {
   checkArg(path, "character", len = 1L, na.ok = FALSE)
-  
+
   # do not warn about EOL
   lines = readLines(file.path(path, "description.txt"), warn = FALSE)
   lines = str_split(lines, ":")
   desc = as.list(str_trim(sapply(lines, function(x) x[2])))
   names(desc) = str_trim(sapply(lines, function(x) x[1]))
-  
+
   # now handle all non-scalar strings and convert them to proper data types
-  
+
   convertField = function(name, cast = as.character) {
     val = str_trim(desc[[name]])
     val = if (length(val) == 0L || val == "")
