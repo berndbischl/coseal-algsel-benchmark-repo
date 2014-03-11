39df735a3495522f1b8984cc4b669bf10e54158d
Bernd Bischl <bernd_bischl@gmx.net>
2014-03-11 02:34:20 +0100

    clean up

--- algselbench/R/summarizeFeatureSteps.R
+++ algselbench/R/summarizeFeatureSteps.R
@@ -8,7 +8,7 @@ summarizeFeatureSteps= function(astask) {
   checkArg(astask, "ASTask")
   rs = astask$feature.runstatus
   # First 2 cols are instanceid and rep
-  # create prop table for the subsequent steo columns over their levels
+  # create prop table for the subsequent step columns over their levels
   n = ncol(rs)
   res = dapply(rs[, 3:n], function(x)
     100 * prop.table(table(x)))
@@ -16,13 +16,15 @@ summarizeFeatureSteps= function(astask) {
   size = sapply(astask$desc$feature_steps, length)
   res = setColNames(t(res), levels(rs[, 3]))
   res = cbind(size = size, res)
-  fc = astask$feature.costs[, 3:n]
-  res2 = t(sapply(fc, function(x) c(
-    cost.min = min(x, na.rm = TRUE), 
-    cost.mean = mean(x, na.rm = TRUE), 
-    cost.max = max(x, na.rm = TRUE),
-    cost.na = mean(is.na(x))
-  )))
-  res = cbind(res, res2)
+  fc = astask$feature.costs
+  if (!is.null(fc)) {
+    res2 = t(sapply(fc[, 3:n], function(x) c(
+      cost.min = min(x, na.rm = TRUE),
+      cost.mean = mean(x, na.rm = TRUE),
+      cost.max = max(x, na.rm = TRUE),
+      cost.na = mean(is.na(x))
+    )))
+    res = cbind(res, res2)
+  }
   return(res)
 }
