bbbdf988b5f48ea3e5c96138aaa3d7c8441a94ad
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-03-11 21:31:05 +0100

    clean up uunit test

--- algselbench/inst/tests/test_runLlamaModels.R
+++ algselbench/inst/tests/test_runLlamaModels.R
@@ -1,6 +1,15 @@
 context("runLlamaModels")
 
 test_that("runLlamaModels", {
-  r1 = runLlamaModels(testtask1, nfolds = 2L, stratify = TRUE)
-  r2 = runLlamaModels(testtask1, nfolds = 2L, stratify = FALSE)
-})
+  cv = createCVSplits(testtask1, folds = 2, reps = 1L)
+  runLlamaModels(testtask1, 
+    baselines = "vbs", 
+    classifiers = "J48",
+    regressors = character(0),
+    clusterers = character(0)
+  )
+  submitJobs(reg)
+  waitForJobs(reg)
+  res = reduceResultsExperiments(reg)
+  expect_true(is.data.frame(res) && nrow(res) == 2L)
+)
