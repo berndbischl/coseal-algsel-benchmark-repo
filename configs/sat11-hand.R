7a2042feeec1ff402c9e94b7d1060824a0a55433
Bernd Bischl <bernd_bischl@gmx.net>
2014-03-14 01:33:51 +0100

    add default feat steps for SAT

--- configs/sat11-hand.R
+++ configs/sat11-hand.R
@@ -1,3 +1,5 @@
 algo.perf.boxplots.log = TRUE
 algo.perf.densities.log = TRUE
 algo.perf.scatter.log = TRUE
+
+feature.steps.default = c("Pre_featuretime", "Basic_featuretime", "KLB_featuretime", "CG_featuretime") 
--- configs/sat12-hand.R
+++ configs/sat12-hand.R
@@ -2,3 +2,5 @@ algo.perf.boxplots.log = TRUE
 algo.perf.densities.log = TRUE
 algo.perf.scatter.log = FALSE
 
+feature.steps.default = c("Pre_featuretime", "Basic_featuretime", "KLB_featuretime", "CG_featuretime") 
+
