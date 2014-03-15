a993adf6268d9b61c29e407203900da7274c8eaa
Bernd Bischl <bernd_bischl@gmx.net>
2014-03-15 16:21:58 +0100

    clean up

--- configs/sat11-hand.R
+++ configs/sat11-hand.R
@@ -2,4 +2,4 @@ algo.perf.boxplots.log = TRUE
 algo.perf.densities.log = TRUE
 algo.perf.scatter.log = TRUE
 
-feature.steps.default = c("Pre_featuretime", "Basic_featuretime", "KLB_featuretime", "CG_featuretime") 
+feature.steps.default = c("Pre_featuretime", "Basic_featuretime", "KLB_featuretime", "CG_featuretime")
--- configs/sat11-indu.R
+++ configs/sat11-indu.R
@@ -2,5 +2,5 @@ algo.perf.boxplots.log = TRUE
 algo.perf.densities.log = TRUE
 algo.perf.scatter.log = TRUE
 
-feature.steps.default = c("Pre_featuretime", "Basic_featuretime", "KLB_featuretime", "CG_featuretime") 
+feature.steps.default = c("Pre_featuretime", "Basic_featuretime", "KLB_featuretime", "CG_featuretime")
 
