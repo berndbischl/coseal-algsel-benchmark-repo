bf2660a31a1a1e15999a1b145e35b14112cd3c5f
Bernd Bischl <bernd_bischl@gmx.net>
2014-04-09 22:06:14 +0200

    reoder data in parseASTask to reduce potential mistakes later

--- algselbench/R/parseASTask.R
+++ algselbench/R/parseASTask.R
@@ -1,4 +1,57 @@
-#' Parses description file and returns an S3 class of the contents
+#' @title Parses the data files of an algorithm selection task int an S3 object.
+#'
+#' @description
+#'
+#' Object members
+#'
+#' Let n be the number of (replicated) instances, m the number of unique instances,
+#' p the number of features, s the number of steps and k the number of algorithms.
+#'
+#' \describe{
+#' \item{desc [\code{\link{ASTaskDesc}}]}{Description object, containing further info.}
+#' \item{feature.runstatus [\code{data.frame(n, s + 2)}]}{Runstatus of feature computation steps.
+#'   The first 2 columns are \dQuote{instance_id} and \dQuote{rep}, the remaining are the status factors.
+#'   The step columns are in the same order as the feature steps in the description object.
+#'   The factor levels are always: ok, presolved, crash, timeout, memout, other.
+#'   No entry can be \code{NA}.
+#'   The data.frame is sorted by \dQuote{instance_id}, then \dQuote{repetition}.}
+#' \item{feature.costs [\code{data.frame(n, s + 2)}]}{Costs of feature computation steps.
+#'   The first 2 columns are \dQuote{instance_id} and \dQuote{repetition}, the remaining are
+#'   numeric costs of the feature steps.
+#'   The step columns are in the same order as the feature steps in the description object.
+#'   code{NA} means the cost is not available, possibly because the feature computation was aborted.
+#'   The data.frame is sorted by \dQuote{instance_id}, then \dQuote{repetition}.}
+#'   If no cost file is available at all, \code{NULL} is stored.
+#' \item{feature.values [\code{data.frame(n, p + 2)}]}{Measured feature values.of instances.
+#'   The first 2 columns are \dQuote{instance_id} and \dQuote{rep}. The remaining ones are
+#'   the measured instance features.
+#'   The feature columns are in the same order as \dQuote{features_deterministic},
+#'   \dQuote{features_stochastic} in the description object.
+#'   code{NA} means the feature is not available, possibly because the feature computation was aborted.
+#'   The data.frame is sorted by \dQuote{instance_id}, then \dQuote{repetition}.}
+#' \item{algo.runstatus [\code{data.frame(n, s + 2)}]}{Runstatus of algorithm runs.
+#'   The first 2 columns are \dQuote{instance_id} and \dQuote{rep}, the remaining are the status factors.
+#'   The step columns are in the same order as the feature steps in the description object.
+#'   The factor levels are always: ok, presolved, crash, timeout, memout, other.
+#'   No entry can be \code{NA}.
+#'   The data.frame is sorted by \dQuote{instance_id}, then \dQuote{repetition}.}
+#' \item{algo.runstatus [\code{data.frame(n, k + 2)}]}{Measured feature values.of instances.
+#'   The first 2 columns are \dQuote{instance_id} and \dQuote{rep}. The remaining ones are
+#'   the measured instance features.
+#'   The feature columns are in the same order as \dQuote{features_deterministic},
+#'   \dQuote{features_stochastic} in the description object.
+#'   code{NA} means the feature is not available, possibly because the feature computation was aborted.
+#'   The data.frame is sorted by \dQuote{instance_id}, then \dQuote{repetition}.}
+#' \item{cv.splits[\code{data.frame(m, 3)}]}{Definition of cross-validation splits for each replication
+#'   of a repeated CV with folds.
+#'   Has columns \dQuote{instance_id}, \dQuote{repetition} and \dQuote{fold}.
+#'   The instances with fold = i for a replication r constitute the i-th test set for the r-th CV.
+#'   The training set is the \dQuote{instance_id} column with repetition = r, in the same order,
+#'   when the test set is removed.
+#'   The data.frame is sorted by \dQuote{repetition}, then \dQuote{fold}, then \dQuote{instance_id}}
+#'   If no CV file is available at all, \code{NULL} is stored, and a warning is issued, although this
+#'   should not happen.
+#' }
 #'
 #' @param path [\code{character(1)}]\cr
 #'   Path to directory of benchmark data set.
@@ -10,27 +63,52 @@ parseASTask = function(path) {
 
   desc = parseDescription(path)
   fsteps = names(desc$feature_steps)
+
+  ### build feature.runstatus
   feature.runstatus = read.arff(file.path(path, "feature_runstatus.arff"))
   # make sure we have correct levels
   for (j in 3:ncol(feature.runstatus)) {
     feature.runstatus[, j] = factor(feature.runstatus[, j],
       levels = c("ok", "timeout", "memout", "presolved", "crash", "other"))
   }
-  # make sure step run status is in the correct order of feature steps
+  # sort rows and cols
   feature.runstatus = feature.runstatus[, c("instance_id", "repetition", fsteps)]
-  feature.values = read.arff(file.path(path, "feature_values.arff"))
+  feature.runstatus = sortByCol(feature.runstatus, c("instance_id", "repetition"))
+
+  ### build feature.costs
   costfile = file.path(path, "feature_costs.arff")
   if (file.exists(costfile)) {
     feature.costs = read.arff(file.path(path, "feature_costs.arff"))
-    # make sure step costs is in the correct order of feature steps
+    # sort rows and cols
     feature.costs = feature.costs[, c("instance_id", "repetition", fsteps)]
+    feature.costs = sortByCol(feature.costs, c("instance_id", "repetition"))
   } else {
     feature.costs = NULL
   }
+
+  ### build feature.values
+  feature.values = read.arff(file.path(path, "feature_values.arff"))
+  # sort rows and cols
+  feature.values = feature.values[, c("instance_id", "repetition",
+    desc$features_deterministic, desc$features_stochastic)]
+  feature.values = sortByCol(feature.values, c("instance_id", "repetition"))
+
   algo.runs = read.arff(file.path(path, "algorithm_runs.arff"))
+
+  ### build algo.runstatus
+  # sort rows and cols
+  algo.runstatus = algo.runs[, c("instance_id", "repetition", "algorithm", "runstatus")]
+  algo.runstatus = algo.runstatus[, c("instance_id", "repetition",
+    desc$algorithms_deterministic, algorithms_stochastic)]
+  algo.runstatus = sortByCol(algo.runstatus, c("instance_id", "repetition"))
+
+  ### build cv.splits
   cv.file = file.path(path, "cv.arff")
   if (file.exists(cv.file)) {
     cv.splits = read.arff(cv.file)
+    # sort rows and cols
+    cv.splits = cv.splits[, c("instance_id", "repetition", "fold")]
+    cv.splits = sortByCol(cv.splits, c("repetition", "fold", "instance_id"))
   } else {
     warningf("No cv file exists for task at:\n%s", path)
     cv.splits = NULL
@@ -39,9 +117,10 @@ parseASTask = function(path) {
   makeS3Obj("ASTask",
     desc = desc,
     feature.runstatus = feature.runstatus,
-    feature.values = feature.values,
     feature.costs= feature.costs,
-    algo.runs = algo.runs,
+    feature.values = feature.values,
+    algo.runstatus = algo.runstatus,
+    algo.perf = algo.perf,
     cv.splits = cv.splits
   )
 }
