841d1195279f2addd81926b73789d91c2c90b54a
Bernd Bischl <you@example.com>
2014-04-18 18:44:36 +0200

    clean up

--- algselbench/R/checkLogarithm.R
+++ algselbench/R/checkLogarithm.R
@@ -1,5 +1,5 @@
 # throws an exception if one tries to calculate the logarithm of non-negative values
 checkLogarithm = function(x, log) {
   if (log && (any(x <= 0)))
-    stop("Can't compute logarithm of non-negative values.")
+    stop("Cannot compute logarithm of <= 0 value! ")
 }
