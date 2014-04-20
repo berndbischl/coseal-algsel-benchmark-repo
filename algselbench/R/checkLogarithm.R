138d9e7a902ad4e65f0b56153f45b9aed8b2fcd2
Bernd Bischl <you@example.com>
2014-04-20 21:00:26 +0200

    clean up

--- algselbench/R/checkLogarithm.R
+++
@@ -1,9 +0,0 @@
-# throws an exception if one tries to calculate the logarithm of non-negative values
-checkLogarithm = function(log, x, col) {
-  if (log) {
-    if (!missing(col))
-      x = x$col
-    if ((any(x <= 0)))
-      stop("Cannot compute logarithm of <= 0 value! ")
-  }
-}
--- algselbench/R/utils.R
+++ algselbench/R/utils.R
@@ -5,3 +5,14 @@ checkMeasure = function(measure, desc) {
     checkArg(measure, choices = desc$performance_measures)
   return(measure)
 }
+
+# throws an exception if one tries to calculate the logarithm of non-negative values
+checkLogarithm = function(log, x, col) {
+  if (log) {
+    if (!missing(col))
+      x = x$col
+    if ((any(x <= 0)))
+      stop("Cannot compute logarithm of <= 0 value! ")
+  }
+}
+
