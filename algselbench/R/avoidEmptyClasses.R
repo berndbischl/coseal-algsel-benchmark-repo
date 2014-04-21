9b8b602a5dff5e6007ed0dbb2a34a29ff933e86f
Bernd Bischl <you@example.com>
2014-04-21 20:25:55 +0200

    clean up

--- algselbench/R/avoidEmptyClasses.R
+++
@@ -1,7 +0,0 @@
-avoidEmptyClasses = function(foldedTask) {
-  foldedTask$train = lapply(foldedTask$train, function(data) {
-    data$best = as.factor(as.character(data$best))
-    return(data)
-  })
-  return(foldedTask)
-}
