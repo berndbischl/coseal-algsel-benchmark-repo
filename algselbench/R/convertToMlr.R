9fd696fd780c7bbb00b0f095c79679ef5fd7feca
Bernd Bischl <you@example.com>
2014-04-21 17:44:05 +0200

    clean up

--- algselbench/R/convertToMlr.R
+++ algselbench/R/convertToMlr.R
@@ -5,15 +5,16 @@ convertToMlr = function(astask, measure, feature.steps, add.feature.costs = TRUE
   desc = astask$desc
   measure = ch$measure; feature.steps = ch$feature.steps
 
-  feats = convertFeats(astask, with.id = FALSE)
-  cp = convertPerf(astask, measure, feature.steps, add.feature.costs)
+  feats = convertFeats(astask, with.instance.id = FALSE)
+  cp = convertPerf(astask, measure = measure, feature.steps = feature.steps,
+    add.feature.costs = add.feature.costs, with.instance.id = FALSE)
   costs = if (desc$maximize[[measure]])
     -cp$perf
   else
     cp$perf
-  print(summary(costs))
+  # FIXME: bad, we do it as some cns start with numbers, also in this case mlr error msg is wrong: "special chars"
+  # this problems also occurs in other plcaces, we need to handle it generally
   cns = colnames(costs)
-  # FIXME bad, we do it as some cns start with numbers, also in this case mlr error msg is wrong: "special chars"
   cns = paste0("xx", cns)
   colnames(costs) = cns
   makeCostSensTask(id = desc$task_id, data = feats, costs = as.matrix(costs))
