9c912ce3739d031f6b9934a97addf30c8f6ee935
Bernd Bischl <you@example.com>
2014-04-21 18:20:35 +0200

    clean up

--- algselbench/R/removeConstantFeatures.R
+++
@@ -1,7 +0,0 @@
-removeConstantFeatures = function(task) {
-  task.data = task$data[, task$features]
-  constant.feats = names(which(sapply(task.data, function(x) length(unique(x)) == 1)))
-  task$data = task$data[, setdiff(names(task$data), constant.feats)]
-  task$features = setdiff(task$features, constant.feats)
-  return(task)  
-}
