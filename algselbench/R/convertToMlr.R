b2dd633b2075447f5258cb2f5e63ce515bbc0dcd
Bernd Bischl <you@example.com>
2014-04-21 23:45:02 +0200

    better convertToMlr

--- algselbench/R/convertToMlr.R
+++ algselbench/R/convertToMlr.R
@@ -5,6 +5,7 @@ convertToMlr = function(astask, measure, feature.steps, add.feature.costs = TRUE
   desc = astask$desc
   measure = ch$measure; feature.steps = ch$feature.steps
 
+  ### task
   feats = convertFeats(astask, with.instance.id = FALSE)
   cp = convertPerf(astask, measure = measure, feature.steps = feature.steps,
     add.feature.costs = add.feature.costs, with.instance.id = FALSE)
@@ -17,7 +18,27 @@ convertToMlr = function(astask, measure, feature.steps, add.feature.costs = TRUE
   cns = colnames(costs)
   cns = paste0("xx", cns)
   colnames(costs) = cns
-  makeCostSensTask(id = desc$task_id, data = feats, costs = as.matrix(costs))
+  mlr.task = makeCostSensTask(id = desc$task_id, data = feats, costs = as.matrix(costs))
+
+  ### CV 
+  cv.splits = astask$cv.splits
+  folds = max(cv.splits$fold)
+  n = length(unique(cv.splits$instance_id))
+  rdesc = makeResampleDesc("CV", iters = folds)
+  rin = makeResampleInstance(rdesc, size = n)
+  all = 1:n
+  # these are sorted, like our data
+  all.ids = getInstanceNames(astask)
+  # get position into sorted ids
+  idsToIndices = function(ids) match(ids, all.ids)
+  for (i in 1:folds) {
+    s = subset(cv.splits, fold == i)
+    test.ids = s$instance_id
+    train.ids = setdiff(cv.splits$instance_id, test.ids)
+    rin$train.inds[[i]] = idsToIndices(train.ids)
+    rin$test.inds[[i]] = idsToIndices(test.ids)
+  }
+  list(mlr.task = mlr.task, mlr.rin = rin)
 }
 
 
