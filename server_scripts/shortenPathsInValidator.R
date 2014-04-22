39c9e76211e7b407978d3c83c5b55f8bac8f2f47
Pascal Kerschke <pascal.kerschke@wi.uni-muenster.de>
2014-04-22 14:47:46 +0200

    documented shortenPathsInValidator.R

--- server_scripts/shortenPathsInValidator.R
+++ server_scripts/shortenPathsInValidator.R
@@ -1,5 +1,10 @@
 #' Rename the directory names of the output from the validator script.
 #'
+#' @description
+#' Refurbishes the output of the validator script by concealing the path to the
+#' files of the coseal directory.
+#' Also, assures that the alignment of the output is consistent.
+#' 
 #' @param s [\code{list(2)}]\cr
 #'   List, containing the output of the validator script.
 #' @param task.dir [\code{character(2)}]\cr
@@ -15,17 +20,24 @@ shortenPathsInValidator = function(s, task.dir) {
   files = c("ground_truth.arff", "citation.bib", "description.txt", 
     "algorithm_runs.arff", "feature_values.arff", "feature_runstatus.arff",
     "feature_costs.arff")
+  # only list the local files, but not the entire path
   for (file in files) {
     index = grep(file, output)
     output[index] = sapply(index, function(i) 
       paste(strsplit(output[[i]], paste(task.dir, "/", sep = ""))[[1]], collapse = ""))
   }
-  index = which(sapply(output, function(ou) grepl("% Instances: \t\t", ou)))
-  output[[index]] = paste(strsplit(output[[index]], "\t\t")[[1]], collapse = "\t\t\t")
-  index = which(sapply(output, function(ou) grepl("% Valid Instances: \t", ou)))
-  output[[index]] = paste(strsplit(output[[index]], "\t")[[1]], collapse = "\t\t\t")
-  index = which(sapply(output, function(ou) grepl("% Presolved: \t\t", ou)))
-  output[[index]] = paste(strsplit(output[[index]], "\t\t")[[1]], collapse = "\t\t\t")
+  output = alignOutput(output, "Instances", "\t\t")
+  output = alignOutput(output, "Valid Instances", "\t")
+  output = alignOutput(output, "Presolved", "\t\t")
   s$output = output
   return(s)
+}
+
+
+# help function, assuring that the validator output for certain strings is properly aligned 
+alignOutput = function(output, string, sep) {
+  index = which(sapply(output, function(ou) grepl(sprintf("%% %s: %s", string, sep), ou)))
+  if (length(index) > 0)
+    output[[index]] = paste(strsplit(output[[index]], sep)[[1]], collapse = "\t\t\t")
+  return(output)
 }
