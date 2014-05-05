6e99f2026605f28c6bed380f5487144c39c116b8
Pascal Kerschke <pascal.kerschke@wi.uni-muenster.de>
2014-05-05 15:12:04 +0200

    manually removed NAs in PerfDensities

--- algselbench/R/plotAlgoPerfDensities.R
+++ algselbench/R/plotAlgoPerfDensities.R
@@ -3,8 +3,11 @@
 plotAlgoPerfDensities = function(astask, measure, impute.failed.runs = TRUE, impute.zero.vals = FALSE, log = FALSE) {
   z = getEDAAlgoPerf(astask, measure, impute.failed.runs = impute.failed.runs, jitter = TRUE,
     impute.zero.vals = impute.zero.vals, check.log = log, format = "long", with.instance.id = FALSE)
+  # FIXME: had to remove failed runs manually as na.rm does not work properly
+  if (!impute.failed.runs)
+    z$data = z$data[!is.na(z$data[,z$measure]), ]
   p = ggplot(z$data, aes_string(x = z$measure, col = "algorithm"))
-  # FIXME: It still see warnings here? na.rm n the next line should disable them?
+  # FIXME: I still see warnings here? na.rm in the next line should disable them?
   p = p + geom_density(na.rm = TRUE)
   if (log)
     p = p + scale_x_log10()
