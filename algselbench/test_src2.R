d5b6a0fe31b251081310385bdcc9fc9163d514aa
Bernd Bischl <bernd_bischl@gmx.net>
2014-10-30 19:54:51 +0100

    clean up

--- algselbench/test_src2.R
+++ algselbench/test_src2.R
@@ -1,14 +1,23 @@
 library(llama)
 library(RWeka)
-# load_all("~/cos/mlr")
 load_all(".")
 
 
-ast = parseASScenario("/home/bischl/cos/coseal/data/QBF_2011")
-lt = convertToLlamaCVFolds(ast, add.feature.costs = F)
-lres = classify(classifier = J48, data = lt)
-mcps1 = unlist(misclassificationPenalties(lt, lres$predictions))
-mcp1 = mean(mcps1)
-print(summary(mcps1))
-print(mcp1)
+ast = parseASScenario("/home/bischl/cos/coseal/data/QBF-2011")
+reg = runLlamaModels(ast, baselines = c("vbs"),
+  # classifiers = makeLearner("classif.rpart"),
+  # regressors = makeLearner("regr.lm"),
+  # clusterers = makeLearner("cluster.SimpleKMeans")
+)
+submitJobs(reg)
+waitForJobs(reg)
+d = reduceResultsExperiments(reg)
+# z = testJob(reg, 1, external = T)
+
+# lt = convertToLlamaCVFolds(ast, add.feature.costs = F)
+# lres = classify(classifier = J48, data = lt)
+# mcps1 = unlist(misclassificationPenalties(lt, lres$predictions))
+# mcp1 = mean(mcps1)
+# print(summary(mcps1))
+# print(mcp1)
 
